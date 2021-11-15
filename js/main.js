ae551f61cb5b61783585afefb63f8ab1fbee5ab3
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-15 15:38:36 +0100

    add deleteTodo()

--- js/main.js
+++ js/main.js
@@ -15,8 +15,34 @@
 
     MILESTONE 3
     Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-    
+
     Bonus:
     1. Oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista.
     2. Cliccando sul testo del todo, invertire il valore della proprietà completed del todo corrispondente (se completed era uguale a false, impostare true e viceversa).
 **/
+
+const app = new Vue({
+    el: '#todoListApp',
+    data: {
+        todos: [
+            {
+                text: 'comprare il pane',
+                completed: true,
+            },
+            {
+                text: 'comprare il pomodoro',
+                completed: false,
+            },
+            {
+                text: 'andare dal dentista',
+                completed: true,
+            },
+        ],
+        newTodo = '',
+    },
+    methods: {
+        deleteTodo(index){
+            this.todos.splice(index, 1)
+        },
+    }
+});
