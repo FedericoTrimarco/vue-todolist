ea4c2648be3b80f7c4b981be10659f8fc65cb391
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-16 02:47:37 +0100

    fix-js-1

--- js/main.js
+++ js/main.js
@@ -46,7 +46,8 @@ const app = new Vue({
         }, 
         addNewTodo(){
             if(this.newTodo != ''){
-                this.todos.unshift({
+                this.todos.unshift(
+                {
                     text: this.newTodo,
                     completed: false,
                 });
